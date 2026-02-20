4fbdd46f489d1f2816d768d96f546ee61b8cec61
wesleyleandroifmg <wesleyleandross@gmail.com>
2026-02-20 19:00:46 -0300

    form editar implementado

--- produtos/views.py
+++ produtos/views.py
@@ -1,4 +1,4 @@
-from urllib import request
+from django.contrib import messages # Importa o módulo de mensagens para exibir mensagens de sucesso ou erro para o usuário
 from django.shortcuts import render # Importa a função render para renderizar templates
 from .models import Produto, Categoria # Importa o modelo Produto
 from django.shortcuts import get_object_or_404 # Importa a função get_object_or_404 para lidar com objetos não encontrados
@@ -44,6 +44,7 @@ def editar_produto(request, id):
     # Busca o objeto no banco, se não encontrar, retorna 404 (página não encontrada)
     post = get_object_or_404(Produto, pk=id)
 
+
     # Se for POST, atualiza o objeto, só que agora, ao invés de criar um novo, liga o formulário ao objeto existente usando o parâmetro instance
     if request.method == "POST":
         
@@ -54,14 +55,20 @@ def editar_produto(request, id):
         if form.is_valid():
             # Atualiza o objeto existente
             form.save()
+
             # Redireciona para a página de detalhes do produto editado
-            return redirect("produtos:detalhe", pk=id)
+            # Mensagem de sucesso
+            messages.success(request, "Produto salvo com sucesso!")
+
+            # Redirect (parte essencial do PRG)
+            return redirect("produtos:lista")
 
     else:
         # Se for GET, apenas mostra os dados já preenchidos
         form = ProdutoForm(instance=post)
 
     return render(request, "produtos/produtoForm.html", {"form": form})
+
 
 
 def inserir_categoria(request):
