0cbc4545b3c565a6762acd1f52bae20279143422
wesleyleandroifmg <wesleyleandross@gmail.com>
2026-03-20 19:51:59 -0300

    .gitignore e setup_admin.py

--- setup_admin.py
+++ setup_admin.py
@@ -11,12 +11,14 @@ User = get_user_model()
 
 # Defina as credenciais iniciais (os alunos podem mudar depois)
 username = 'admin'
+firstname = 'Admin'
+lastname = 'User'
 email = 'teste@teste.com'
 password = 'SenhaDificil123'
 
 if not User.objects.filter(username=username).exists():
     print(f"Criando superusuário: {username}...")
-    User.objects.create_superuser(username, email, password)
+    User.objects.create_superuser(username, email, password, first_name=firstname, last_name=lastname)
     print("Superusuário criado com sucesso!")
 else:
     print("Superusuário já existe.")
